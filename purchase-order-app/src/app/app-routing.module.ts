ec6d2db6572c3358a9fb8e27398469889cb1ae6f
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-11-16 14:39:27 +0530

    add orders module

--- purchase-order-app/src/app/app-routing.module.ts
+++ purchase-order-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
