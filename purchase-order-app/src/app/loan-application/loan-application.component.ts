4aab8878f8e1aacf078f2932eb2ca10d3b155c12
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-11-26 17:33:46 +0530

    preview updates

--- purchase-order-app/src/app/loan-application/loan-application.component.ts
+++ purchase-order-app/src/app/loan-application/loan-application.component.ts
@@ -1,3 +1,4 @@
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -8,8 +9,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoanApplicationComponent implements OnInit {
   isLinear = false;
+  shouldPreview = false;
   @ViewChild('loanInformationFormRef') loanInformationFormRef: ElementRef;
   @ViewChild('applicantInfoFormRef') applicantInfoFormRef: ElementRef;
+
+  @ViewChild('preLoanInformationFormRef') preLoanInformationFormRef: ElementRef;
+  @ViewChild('preApplicantInfoFormRef') preApplicantInfoFormRef: ElementRef;
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -18,12 +23,28 @@ export class LoanApplicationComponent implements OnInit {
   }
 
   loanInformationFormOnloadHandler(eventData) {
-     this.hideRecordFormHeader(this.loanInformationFormRef);
+    this.hideRecordFormHeader(this.loanInformationFormRef);
   }
 
   applicantInformationFormOnloadHandler(eventData) {
     this.hideRecordFormHeader(this.applicantInfoFormRef);
- }
+  }
+  preLoanInformationFormOnloadHandler(eventData) {
+    this.hideRecordFormHeader(this.preLoanInformationFormRef);
+    this.previewLoanInfo();
+  }
+
+  preApplicantInformationFormOnloadHandler(eventData) {
+    this.hideRecordFormHeader(this.preApplicantInfoFormRef);
+    this.previewApplicantInfo();
+  }
+
+
+
+  stepSelectionChange(stepChange: StepperSelectionEvent) {
+    this.shouldPreview = stepChange.selectedStep.label === "Preview & Submit";
+    
+  }
 
   /**
    * Hide the default tabs from ace-record-form
@@ -34,5 +55,20 @@ export class LoanApplicationComponent implements OnInit {
     const headerDomElement = formRef.nativeElement.querySelector('mat-tab-header');
     headerDomElement.style.display = hideHeaderTabs ? 'none' : "inherit";
   }
+  private previewLoanInfo() {
+    const loanInfoFormDomElement = this.loanInformationFormRef.nativeElement;
+    const previewLoaninfoFormDomElement = this.preLoanInformationFormRef.nativeElement;
+    const formData = loanInfoFormDomElement.getRecordData();
+    previewLoaninfoFormDomElement.setRecordData(formData);
+    console.log(loanInfoFormDomElement, previewLoaninfoFormDomElement, formData);
+
+  }
+  private previewApplicantInfo() {
+    const applicantInfoDomElement = this.applicantInfoFormRef.nativeElement;
+    const previewApplicantInfoFormDomElement = this.preApplicantInfoFormRef.nativeElement;
+    const formData = applicantInfoDomElement.getRecordData();
+    previewApplicantInfoFormDomElement.setRecordData(formData);
+  }
+
 
 }
