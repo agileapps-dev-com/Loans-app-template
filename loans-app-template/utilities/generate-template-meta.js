87dde8e2f38311c4fc63812015dcd526c497dff2
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-12-02 15:00:14 +0530

    update template meta generator script

--- loans-app-template/utilities/generate-template-meta.js
+++ loans-app-template/utilities/generate-template-meta.js
@@ -1,47 +1,56 @@
+
 const jsonfile = require('jsonfile');
 const packageFileSrc = './package.json';
 let destitnationFile = null;
 
-const nameSuffix = '-tmpl';
-const templateMetaObj = {};
 
-let packageName = null,
-    packageVersion = null;
+module.exports.init = function () {
+
+    const nameSuffix = '-tmpl';
+    const templateMetaObj = {};
+
+    let packageName = null,
+        packageVersion = null;
 
 
-const templateMeta = {
-    getTemplateNameAndVersion() {
+
+    function getTemplateNameAndVersion() {
         return jsonfile.readFile(packageFileSrc)
             .then(obj => {
                 packageName = obj['name'] || null;
                 packageVersion = obj['version'] || null;
-                destitnationFile = "./dist/" + packageName + "/template-details.json"
+                destitnationFile = "./dist/" + packageName + "/template-details.json";
+                console.log(`******** Generating CUI template meta data for ${packageName + nameSuffix}  (Version ${packageVersion}) ******** 
+                `);
             })
             .catch(error => console.error(error))
-    },
-    setTemplateMeta() {
+    };
+
+    function setTemplateMeta() {
         if (packageName && packageVersion) {
             templateMetaObj['name'] = packageName + nameSuffix;
             templateMetaObj['version'] = packageVersion;
         }
 
-    },
-    createtemplateMetaJsonFile() {
+    };
+
+    function createtemplateMetaJsonFile() {
         if (destitnationFile) {
             jsonfile.writeFile(destitnationFile, templateMetaObj, { spaces: 2 }, function (err) {
                 if (err) console.error(err)
-            })
+            });
+            console.log(`******** Template meta for ${packageName + nameSuffix} (${packageVersion}) generated successfully! ******** 
+             `);
+
         }
 
     }
-}
 
-module.exports.init = function () {
 
-    // readTemplateVersion();
-    templateMeta.getTemplateNameAndVersion().then(() => {
-        templateMeta.setTemplateMeta();
-        templateMeta.createtemplateMetaJsonFile();
+    getTemplateNameAndVersion().then(() => {
+        setTemplateMeta();
+        createtemplateMetaJsonFile();
     })
 };
+
 this.init();
